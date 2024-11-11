4cd30591f96e7b94e2d13e477b2d27aebf2dbc84
DESKTOP-JJ86S6U\BreezyWeezy <eddinetaki087@gmail.com>
2024-11-11 21:17:24 +0100

    new changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    id("com.google.gms.google-services")
 }
 
 android {
@@ -51,15 +50,11 @@ android {
 
 dependencies {
     implementation (libs.kotlinx.coroutines.android)
-    implementation (libs.auth.google.auth.library.oauth2.http)
-    implementation (libs.okhttp)
-    implementation (libs.firebase.messaging)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.firebase.database.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
